ed02e6328baca76bff13c7a63b449a0c6674ba73
ctn <35670719+nguyenchloet@users.noreply.github.com>
2023-02-21 09:36:47 -0500

    Updated code

--- 2048.cpp
+++ 2048.cpp
@@ -16,22 +16,6 @@ int board[BOARD_SIZE][BOARD_SIZE];
 // copy of the gameboard for change comparison
 int _board[BOARD_SIZE][BOARD_SIZE];
 
-class Game {
-    public:
-        Game();
-        ~Game();
-        void Play();
-
-    private:
-        //Variables                                                                                                                                               
-
-        //Functions    
-        void InitializeBoard();    
-        void PrintBoard();   
-        void CommandLoop();    
-        void AddTile();                                                                                                                                
-};
-
 Game::Game() {}
 
 Game::~Game() {}
@@ -40,6 +24,8 @@ Game::~Game() {}
 // otherwise, the tiles will remain 0 value
 void Game::InitializeBoard() {
     srand(time(NULL));
+    score = 0;
+
     std::cout << "\n\tWelcome to 2048!" << std::endl;
     //ask user if they want to read the rules?
 
@@ -106,31 +92,27 @@ void Game::PrintBoard(){
 void Game::CommandLoop() {
     char user_choice;
     do {
-        std::cout << "Press: (w):up (a):left (d):right (s):down (q): quit " << std::endl;
+        std::cout << "Press: (a):left (d):right (w):up (s):down (q): quit " << std::endl;
         std::cin >> user_choice;
         std::cin.ignore();
         if (user_choice == LEFT) {
-            //shift_left();
-            //update_score();
-            AddTile();
+            ShiftLeft();
+            UpdateScore();
             PrintBoard();
         }
         else if (user_choice == RIGHT) {
-            //shift_right();
-            //update_score();
-            AddTile();
+            ShiftRight();
+            UpdateScore();
             PrintBoard();
         }
         else if (user_choice == UP) {
-            //shift_up();
-            //update_score();
-            AddTile();            
+            ShiftUp();
+            UpdateScore();
             PrintBoard();
         }
         else if (user_choice == DOWN) {
-            //shift_down();
-            //update_score();
-            AddTile();
+            ShiftDown();
+            UpdateScore();
             PrintBoard();
         }
         else if (user_choice == QUIT) {
@@ -142,6 +124,13 @@ void Game::CommandLoop() {
         }
     } while (user_choice != QUIT);
     std::cout << "\nGame Over." << std::endl;
+}
+
+/*                                                                                                                                                                
+ * Update score in own function                                                                                                                                   
+ */
+void Game::UpdateScore() {
+  std::cout << "Score: " << score << std::endl;
 }
 
 // add one tile to a random empty position on the board
@@ -177,15 +166,234 @@ void Game::AddTile() {
             } 
         }
     }
-    
-    
-    
+}
+
+/*                                                                                                                                                                
+ * Shift tile with value to left if: tile to left is empty / 0                                                                                                    
+ * Merge value if tile to left is of equal value                                                                                                                  
+ */
+void Game::ShiftLeft() {
+  bool moved = false;
+  for (int row = 0; row < BOARD_SIZE; row++) {
+    for (int col = 0; col < BOARD_SIZE; col++) {
+        for (int curr_col = col + 1; curr_col < BOARD_SIZE; curr_col++) {
+          if ((board[row][col] == 0) and (board[row][curr_col] != 0) and (curr_col < BOARD_SIZE)) {
+            board[row][col] = board[row][curr_col];
+            board[row][curr_col] = 0;
+            moved = true;
+        }
+      }
+    }
+  }
+  //merge tiles LEFT                                                                                                                                              
+  for (int row = 0; row < BOARD_SIZE; row++) {
+    for (int col = 0; col < BOARD_SIZE; col++) {
+      for (int curr_col = col + 1; curr_col < BOARD_SIZE; curr_col++) {
+        if ((board[row][col] != 0) and (board[row][col] == board[row][curr_col]) and (curr_col < BOARD_SIZE)) {
+            board[row][col] += board[row][col + 1];
+            board[row][col + 1] = 0;
+            score += board[row][col];
+        }
+      }
+    }
+  }
+  if (moved == true) {
+    AddTile();
+  }
+}
+
+/*                                                                                                                                                                
+ * Shift tile with value right if: tile to right is empty / 0                                                                                                     
+ * Merge value if tile to right is of equal value                                                                                                                 
+ */
+void Game::ShiftRight() {
+  bool moved = false;
+  for (int row = 0; row < BOARD_SIZE; row++) {
+    for (int col = BOARD_SIZE - 1; col >= 0; col--) {
+      for (int curr_col = col - 1; curr_col >= 0; curr_col--) {
+        if ((board[row][col] == 0) and (board[row][curr_col] != 0) and (curr_col >= 0)) {
+          board[row][col] = board[row][curr_col];
+          board[row][curr_col] = 0;
+          moved = true;
+        }
+      }
+    }
+  }
+  // merge tiles RIGHT                                                                                                                                            
+  for (int row = 0; row < BOARD_SIZE; row++) {
+    for (int col = BOARD_SIZE; col >= 0; col--) {
+      for (int curr_col = col - 1; curr_col >= 0; curr_col--) {
+        if ((board[row][col] != 0) and (board[row][col] == board[row][curr_col]) and (curr_col >= 0)) {
+           board[row][col] += board[row][curr_col];
+           board[row][curr_col] = 0;
+           score += board[row][col];
+        }
+      }
+    }
+  }
+  if (moved == true) {
+    AddTile();
+  }
+}
+
+/*                                                                                                                                                                
+ * While tile + 1 is empty and tile has value, move to tile + 1                                                                                                   
+ * For up and down, columns are the outer for loop                                                                                                                
+ */
+void Game::ShiftUp() {
+  bool moved = false;
+  for (int col = 0; col < BOARD_SIZE; col++) {
+    for (int row = 0; row < BOARD_SIZE; row++) {
+       for (int curr_col = col + 1; curr_col < BOARD_SIZE; curr_col++) {
+         if ((board[col][row] == 0) and (board[curr_col][row] != 0)) {
+           board[col][row] = board[curr_col][row];
+           board[curr_col][row] = 0;
+           moved = true;
+        }
+      }
+    }
+  }
+  // merge tiles UP                                                                                                                                               
+  for (int col = 0; col < BOARD_SIZE; col++) {
+    for (int row = 0; row < BOARD_SIZE; row++) {
+      for (int curr_col = col + 1; curr_col < BOARD_SIZE; curr_col++) {
+        if ((board[col][row] != 0) and (board[col][row] == board[curr_col][row])) {
+          board[col][row] += board[curr_col][row];
+          board[curr_col][row] = 0;
+          score += board[col][row];
+        }
+      }
+    }
+  }
+  if (moved == true) {
+    AddTile();
+  }
+}
+
+/*                                                                                                                                                                
+ * Shift down if tile below is empty and current tile has value                                                                                                   
+ */
+void Game::ShiftDown() {
+  bool moved = false;
+  for (int col = BOARD_SIZE - 1; col >= 0; col--) {
+    for (int row = 0; row < BOARD_SIZE; row++) {
+      for (int curr_col = col - 1; curr_col >= 0; curr_col--) {
+        if ((board[col][row] == 0) and (board[curr_col][row] != 0) and (curr_col >= 0)) {
+          board[col][row] = board[curr_col][row];
+          board[curr_col][row] = 0;
+          moved = true;
+        }
+      }
+    }
+  }
+  //merge tiles DOWN                                                                                                                                              
+  for (int col = BOARD_SIZE - 1; col >= 0; col--) {
+    for (int row = 0; row < BOARD_SIZE; row++) {
+      for (int curr_col = col - 1; curr_col >= 0; curr_col--) {
+        if ((board[col][row] != 0) and (board[curr_col][row] == board[col][row]) and (curr_col >= 0)) {
+          board[col][row] += board[curr_col][row];
+          board[curr_col][row] = 0;
+          score += board[col][row];
+        }
+      }
+    }
+  }
+  if (moved == true) {
+    AddTile();
+  }
+}
+
+/*                                                                                                                                                                
+ * Check if board is full of tiles with value                                                                                                                     
+ */
+bool Game::BoardFull() {
+  int num_empty = 0;
+  for (int i = 0; i < BOARD_SIZE; i++) {
+    for (int j = 0; j < BOARD_SIZE; j++) {
+      if (board[i][j] == 0) {
+        num_empty++;
+      }
+    }
+  }
+
+  if (num_empty == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/*                                                                                                                                                                
+ * Checks if there are any tile of equal value next to each other                                                                                                 
+ * When board is full?                                                                                                                                            
+ */
+bool Game::CanMerge() {
+  //check left                                                                                                                                                    
+  for (int row = 0; row < BOARD_SIZE; row++) {
+    for (int col = 0; col < BOARD_SIZE; col++) {
+      for (int curr_col = col + 1; curr_col < BOARD_SIZE; curr_col++) {
+        if ((board[row][col] != 0) and (board[row][col] == board[row][curr_col]) and (curr_col < BOARD_SIZE)) {
+          return true;
+        }
+      }
+    }
+  }
+  //check right                                                                                                                                                   
+  for (int row = 0; row < BOARD_SIZE; row++) {
+    for (int col = BOARD_SIZE; col >= 0; col--) {
+      for (int curr_col = col - 1; curr_col >= 0; curr_col--) {
+        if ((board[row][col] != 0) and (board[row][col] == board[row][curr_col]) and (curr_col >= 0)) {
+          return true;
+        }
+      }
+    }
+  }
+  //check up                                                                                                                                                      
+  for (int col = 0; col < BOARD_SIZE; col++) {
+    for (int row = 0; row < BOARD_SIZE; row++) {
+      for (int curr_col = col + 1; curr_col < BOARD_SIZE; curr_col++) {
+        if ((board[col][row] != 0) and (board[col][row] == board[curr_col][row])) {
+          return true;
+        }
+      }
+    }
+  }
+  //check down                                                                                                                                                    
+  for (int col = BOARD_SIZE - 1; col >= 0; col--) {
+    for (int row = 0; row < BOARD_SIZE; row++) {
+      for (int curr_col = col - 1; curr_col >= 0; curr_col--) {
+        if ((board[col][row] != 0) and (board[curr_col][row] == board[col][row]) and (curr_col >= 0)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+/*                                                                                                                                                                
+ * End game conditions: over if no empty spots, no merges possible  or score 2048                                                                                 
+ */
+bool Game::GameOver() {
+  if ((BoardFull() == true) && (CanMerge() == false)) {
+    return true;
+  } else if (score == 2048) {
+    return true;
+  } else {
+    return false;
+  }
+  if (true) {
+    std::cout << "Board Full. Game Over!" << std::endl;
+    return 1;
+  }
 }
 
 void Game::Play() {
     InitializeBoard();
     PrintBoard();
-    CommandLoop();
+    while (GameOver() == false) {
+        CommandLoop();
+    }
 }
 
 int main() {
